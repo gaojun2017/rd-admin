bbd4aeed63e1f17dc141fad1bbfce1b8e5846b77
junjun <1648935464@qq.com>
2021-11-21 00:56:49 +0800

    第一次提交

---
+++ src/components/login/login.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="login">
+    <div class="form-box">
+      <el-input v-model="username" placeholder="请输入内容"></el-input>
+      <el-input
+        placeholder="请输入密码"
+        v-model="password"
+        show-password
+      ></el-input>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+};
+</script>
+
+<style scoped>
+.login {
+  width: 100%;
+  height: 100%;
+  background-image: url("../../assets/img/beijing.png");
+  background-size: 100% 100%;
+  background-repeat: no-repeat;
+  background-position: center;
+}
+.form-box {
+  width: 35%;
+  margin: 0 auto;
+  padding-top: 15%;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router/router.js'
+import './assets/css/common.css'
 
+import 'element-ui/lib/theme-chalk/index.css';
+import { Input } from 'element-ui';
+Vue.use(Input);
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
